4ea3ab191cb0ccf5462df3279763502436b4ceb7
ppatil002 <pratikvpatil2002@gmail.com>
2022-05-25 22:16:37 +0530

    Login and Registration

---
+++ app/src/main/java/com/example/zest23/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.zest23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.LinkedHashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
